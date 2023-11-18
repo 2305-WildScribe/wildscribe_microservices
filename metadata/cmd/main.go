e4cc570132e0886b022a3117879bec29fd797c05
Derek Chavez <frrenose17@yahoo.com>
2023-11-18 11:31:40 -0700

    feat: add package main.go

---
+++ metadata/cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"movieexample.com/metadata/internal/controller/metadata"
+	httphandler "movieexample.com/metadata/internal/handler/http"
+	"movieexample.com/metadata/internal/repository/memory"
+)
+
+func main() {
+	log.Println("Starting metadata service...")
+	repo := memory.New()
+	ctrl := metadata.New(repo)
+	h := httphandler.New(ctrl)
+	http.Handle("/metadata", http.HandlerFunc(h.GetMetadata))
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
