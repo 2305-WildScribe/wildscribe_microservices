0f0de9c88a7c8ee7cccfc981e0cbd2f4ebefd34f
Derek Chavez <frrenose17@yahoo.com>
2023-11-19 15:00:52 -0700

    feat: renaming metdata

--- movie/internal/controller/movie/controller.go
+++ movie/internal/controller/movie/controller.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"errors"
 
-	metadatamodel "movieexample.com/metadata/pkg/model"
+	adventuremodel "movieexample.com/adventure/pkg/model"
 	"movieexample.com/movie/internal/gateway"
 	"movieexample.com/movie/pkg/model"
 	ratingmodel "movieexample.com/rating/pkg/model"
@@ -17,30 +17,30 @@ type ratingGateway interface {
 	PutRating(ctx context.Context, recordID ratingmodel.RecordID, recordType ratingmodel.RecordType, rating *ratingmodel.Rating) error
 }
 
-type metadataGateway interface {
-	Get(ctx context.Context, id string) (*metadatamodel.Metadata, error)
+type adventureGateway interface {
+	Get(ctx context.Context, id string) (*adventuremodel.Adventure, error)
 }
 
 // Controller defines a movie service controller.
 type Controller struct {
 	ratingGateway   ratingGateway
-	metadataGateway metadataGateway
+	adventureGateway adventureGateway
 }
 
 // New creates a new movie service controller.
-func New(ratingGateway ratingGateway, metadataGateway metadataGateway) *Controller {
-	return &Controller{ratingGateway, metadataGateway}
+func New(ratingGateway ratingGateway, adventureGateway adventureGateway) *Controller {
+	return &Controller{ratingGateway, adventureGateway}
 }
 
-// Get returns movie details including aggregated rating and movie metadata for a given movie ID.
+// Get returns movie details including aggregated rating and movie adventure for a given movie ID.
 func (c *Controller) Get(ctx context.Context, id string) (*model.MovieDetails, error) {
-	metadata, err := c.metadataGateway.Get(ctx, id)
+	adventure, err := c.adventureGateway.Get(ctx, id)
 	if err != nil && errors.Is(err, gateway.ErrNotFound) {
 		return nil, ErrNotFound
 	} else if err != nil {
 		return nil, err
 	}
-	details := &model.MovieDetails{Metadata: *metadata}
+	details := &model.MovieDetails{Adventure: *adventure}
 	rating, err := c.ratingGateway.GetAggregatedRating(ctx, ratingmodel.RecordID(id), ratingmodel.RecordTypeMovie)
 	if err != nil && !errors.Is(err, gateway.ErrNotFound) {
 		// Just proceed in this case, it's ok not to have ratings yet.
--- movie/pkg/model/model.go
+++ movie/pkg/model/model.go
@@ -1,9 +1,9 @@
 package model
 
-import "movieexample.com/metadata/pkg/model"
+import "movieexample.com/adventure/pkg/model"
 
 // MovieDetails includes movie metadata and its aggregated ratings.
 type MovieDetails struct {
 	Rating	 	*float64 				`json:"rating,omitempty"`
-	Metadata 	model.Metadata 	`json:"metadata"`
+	Adventure 	model.Adventure 	`json:"adventure"`
 }
