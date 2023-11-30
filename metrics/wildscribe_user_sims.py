88ad3cfc29274d91e37132066beb4c4363e4da6a
Ian Lyell <ilyell23@turing.edu>
2023-11-30 17:38:33 -0600

    setup: Setup locust file

--- metrics/wildscribe_user_sims.py
+++ metrics/wildscribe_user_sims.py
@@ -1,6 +1,9 @@
 from locust import HttpUser, task, between
 import random
+import logging
 # from faker import Faker
+
+
 
 class User(HttpUser):
     host = "http://localhost:8080/api/v0"
@@ -10,10 +13,10 @@ class User(HttpUser):
     def on_start(self):
         self.login_index = random.randint(1, 4999)
         self.user_id = None
-        self.LoginUser()
+        self.adventure_id = None
 
     @task
-    def LoginUser(self):
+    def LoginUser(self):  
         if self.user_id is not None:
             return
 
@@ -31,18 +34,19 @@ class User(HttpUser):
         headers = { "Content-Type": "application/json "}
 
         with self.client.post(endpoint, json=payload, headers=headers, catch_response=True) as response:
-            self.user_id = response.json()["data"]["attributes"]["user_id"]
-            if response.status_code == 200:
-                response.success()
-            elif response.status_code != 200:
-                response.failure(f"Request has diffrent status code: {response.status_code}")
-            elif response.elapsed.total_seconds() > 1.0:
+            if response.elapsed.total_seconds() > 1.0:
                 response.failure(f"Request took to long: {response.elapsed.total_seconds()}")
+            elif response.status_code == 200:
+                self.user_id = response.json()["data"]["attributes"]["user_id"]
+                response.success()
             else: 
-                response.failure(f"Request failed with status: {response.reason}")
+                response.failure(f"Request failed : {response.json()}")
    
-    @task
+    @task(6)
     def GetUserAdventures(self):
+        if self.user_id is None:
+            self.LoginUser()
+
         endpoint = "/adventures"
 
         payload = { 
@@ -56,164 +60,107 @@ class User(HttpUser):
         headers = { "Content-Type": "application/json "}
 
         with self.client.post(endpoint, json=payload, headers=headers, catch_response=True) as response:
-            if response.status_code == 200:
+            if response.elapsed.total_seconds() > 1.0:
+                response.failure(f"Request took to long: {response.elapsed.total_seconds()}")
+            elif response.status_code != 200:
+                response.failure(f"Request has diffrent status code: {response.status_code}")
+            elif response.status_code == 200:
+                response.success()
+            else: 
+                response.failure(f"Request failed : {response.json()}")
+   
+    @task(5)
+    def CreateAdventure(self):
+        if self.user_id is None:
+            self.LoginUser()
+
+        endpoint = "/adventure"
+
+        payload = { 
+                    "data":{
+                        "type": "adventures",
+                        "attributes": {
+                            "user_id": self.user_id,
+                            "activity": "Running",
+                            "date": "10/11/2023"
+                        }
+                     }
+                }
+        headers = { "Content-Type": "application/json "}
+
+        with self.client.post(endpoint, json=payload, headers=headers, catch_response=True) as response:
+            if response.elapsed.total_seconds() > 1.0:
+                response.failure(f"Request took to long: {response.elapsed.total_seconds()}")
+            elif response.status_code == 200:
+                self.adventure_id = response.json()["data"]["attributes"][0]["adventure_id"]
                 response.success()
             elif response.status_code != 200:
                 response.failure(f"Request has diffrent status code: {response.status_code}")
-            elif response.elapsed.total_seconds() > 1.0:
-                response.failure(f"Request took to long: {response.elapsed.total_seconds()}")
             else: 
-                response.failure(f"Request failed with status: {response.reason}")
+                response.failure(f"Request failed : {response.json()}")
    
-#     @task(4)
-#     def GetAllTeas(self):
+  
+    @task(2)
+    def UpdateAdventure(self):
 
-#         endpoint = "/teas"
-
-#         with self.client.get(endpoint, catch_response=True) as response:
-#             if response.elapsed.total_seconds() > 1.0:
-#                 response.failure(f"Request took too long: {response.elapsed.total_seconds()} seconds")
-#             elif response.status_code == 200:
-#                 response.success()
-#             else: 
-#                 response.failure("Request Failed")
-
-#     @task(3)
-#     def GetATea(self):
-
-#         tea_id = random.randint(1, 10000)
-
-#         endpoint = f"/teas/{tea_id}"
-
-#         with self.client.get(endpoint, catch_response=True) as response:
-#             response_id = response.json()["data"]["id"]
-#             if response.elapsed.total_seconds() > 1.0:
-#                 response.failure(f"Request took too long: {response.elapsed.total_seconds()} seconds")
-#             elif response.status_code == 200 and tea_id == int(response_id):
-#                 response.success()
-#             elif response.status_code == 410:
-#                 response.success()
-#             else: 
-#                 response.failure(f"Request failed with status: {response.reason}")
-
-#     @task
-#     def GetACustomer(self):
-#         endpoint = "/customer"
-
-#         payload = {
-#             "data":{
-#                 "type": "customer",
-#                 "attributes": {
-#                     "id": self.customer_id
-#                 }
-#             }
-#         }
-
-#         headers = { "Content-Type": "application/json "}
-
-#         with self.client.post(endpoint, json=payload, headers=headers, catch_response=True) as response:
-#             response_id = response.json()["data"]["id"]
-#             if response.status_code == 200 and self.customer_id == int(response_id):
-#                 response.success()
-#             elif response.elapsed.total_seconds() < 1.0:
-#                 response.failure(f"Request took to long: {response.elapsed.total_seconds()}")
-#             else: 
-#                 response.failure(f"Request failed with status: {response.reason}")
-
-#     @task(2)
-#     def Subscribe(self):
-#         endpoint = "/subscribe"
-
-#         if self.set_tea_id == None:
-#             self.set_tea_id = random.randint(1,10000)
-
-#         status = ["pending", "active", "inactive"]
-#         payload = {
-#             "data": {
-#                 "type": "subscription",
-#                 "attributes": {
-#                     "customer_id": self.customer_id,
-#                     "tea_id": self.set_tea_id,
-#                     "status": status[random.randint(0,2)],
-#                     "title": "Earl Grey",
-#                     "price": self.faker.pricetag(),
-#                     "frequency": "Monthly"
-#                 }
-#             }
-#         }
-
-#         headers = {
-#             "Content-Type": "application/json"
-#         }
-
-#         with self.client.post(endpoint, json=payload, headers=headers, catch_response=True) as response:
-#             if response.status_code == 200:
-#                 response.success()
-#             elif response.elapsed.total_seconds() > 1.0:
-#                 response.failure(f"Request took too long: {response.elapsed.total_seconds()} seconds")
-
-
-#     @task(2)
-#     def Unsubcribe(self):
-#         endpoint = "/unsubscribe"
+        if self.user_id is None:
+            return
+        if self.adventure_id is None:
+            return
         
-#         if self.set_tea_id == None:
-#             self.Subscribe()
-        
-#         payload = {
-#             "data": {
-#                 "type": "subscription",
-#                 "attributes": {
-#                     "customer_id": self.customer_id,
-#                     "tea_id": self.set_tea_id
-#                 }
-#             }
-#         }
+        endpoint = "/adventure"
 
-#         headers = {
-#             "Content-Type": "application/json"
-#         }
+        payload = { 
+                    "data":{
+                        "type": "adventures",
+                        "attributes": {
+                            "adventure_id": self.adventure_id,
+                            "user_id": self.user_id,
+                            "activity": "Walking",
+                            "date": "10/11/2023"
+                        }
+                     }
+                }
+        headers = { "Content-Type": "application/json "}
 
-#         with self.client.post(endpoint, json=payload, headers=headers, catch_response=True) as response:
-#             if response.status_code == 200:
-#                 self.set_tea_id = None
-#                 response.success()
-#             elif response.elapsed.total_seconds() > 1.0:
-#                 response.failure(f"Request took too long: {response.elapsed.total_seconds()} seconds")
+        with self.client.patch(endpoint, json=payload, headers=headers, catch_response=True) as response:
+            if response.elapsed.total_seconds() > 1.0:
+                response.failure(f"Request took to long: {response.elapsed.total_seconds()}")
+            elif response.status_code == 200:
+                response.success()
+            elif response.status_code == 500:
+                response.failure(f"Request failed : {response.json()}")
+            else: 
+                response.failure(f"Request failed : {response.json()}")
+    @task(2)
+    def DeleteAdventure(self):
+        if self.adventure_id is None:
+            return
 
+        endpoint = "/adventure"
 
-# class LurkerUser(HttpUser):
-#     host = "http://localhost:3000/api/v0"
-#     wait_time = between(1, 5)
-#     weight = 3
+        payload = { 
+                    "data":{
+                        "type": "adventures",
+                        "attributes": {
+                            "adventure_id": self.adventure_id
+                        }
+                     }
+                }
+        headers = { "Content-Type": "application/json "}
 
-#     @task(2)
-#     def GetAllTeas(self):
+        with self.client.delete(endpoint, json=payload, headers=headers, catch_response=True) as response:
+            if response.elapsed.total_seconds() > 1.0:
+                response.failure(f"Request took to long: {response.elapsed.total_seconds()}")
+            elif response.status_code == 200:
+                self.adventure_id = None
+                response.success()
+            elif response.status_code != 200:
+                response.failure(f"Request has diffrent status code: {response.status_code}")
+            else: 
+                response.failure(f"Request failed : {response.json()}")
 
-#         endpoint = "/teas"
-
-#         with self.client.get(endpoint, catch_response=True) as response:
-#             if response.elapsed.total_seconds() > 1.0:
-#                 response.failure(f"Request took too long: {response.elapsed.total_seconds()} seconds")
-#             elif response.status_code == 200:
-#                 response.success()
-#             else: 
-#                 response.failure("Request Failed")
-
-#     @task
-#     def GetATea(self):
-
-#         tea_id = random.randint(1, 10000)
-
-#         endpoint = f"/teas/{tea_id}"
-
-#         with self.client.get(endpoint, catch_response=True) as response:
-#             response_id = response.json()["data"]["id"]
-#             if response.elapsed.total_seconds() > 1.0:
-#                 response.failure(f"Request took too long: {response.elapsed.total_seconds()} seconds")
-#             elif response.status_code == 200 and tea_id == int(response_id):
-#                 response.success()
-#             elif response.status_code == 410:
-#                 response.success()
-#             else: 
-#                 response.failure(f"Request failed with status: {response.reason}")
+    @task(1)
+    def Logout(self):
+        self.user_id = None
+   
