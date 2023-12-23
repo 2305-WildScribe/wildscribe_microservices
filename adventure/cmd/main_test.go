c829976d3931a59de8f186548018d981052d224a
Ian Lyell <ilyell23@turing.edu>
2023-12-23 16:04:34 -0600

    feat: Add happy path testing for adventure microservice

--- adventure/cmd/main_test.go
+++ adventure/cmd/main_test.go
@@ -2,34 +2,32 @@ package main_test
 
 import (
 	"context"
-
-	// "github.com/stretchr/testify/assert"
-	"log"
-	"net"
-	"os"
-	"testing"
-
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/test/bufconn"
+
+	"log"
+	"net"
+	"os"
+	"testing"
+
 	"wildscribe.com/adventure/internal/controller"
 	grpchandler "wildscribe.com/adventure/internal/handler/grpc"
 	database "wildscribe.com/adventure/internal/repository/mockDB"
 	"wildscribe.com/gen"
 )
 
-// ... (your imports and other code)
-
 const bufSize = 1024 * 1024
 
 var lis *bufconn.Listener
-
 var adventureClient gen.AdventureServiceClient
+var ctx = context.Background()
+
+var adventureId string
 
 func TestMain(m *testing.M) {
-	ctx := context.Background()
 
 	adventureSrv := startAdventureService(ctx)
 	defer adventureSrv.GracefulStop()
@@ -51,7 +49,7 @@ func bufDialer(context.Context, string) (net.Conn, error) {
 }
 
 func startAdventureService(ctx context.Context) *grpc.Server {
-	db := database.MockCollection()
+	db := database.SetupMockDB("adventures")
 
 	log.Println("Setting controller")
 	svc := controller.New(db)
@@ -77,21 +75,103 @@ func startAdventureService(ctx context.Context) *grpc.Server {
 	return srv
 }
 
+func TestCreateAdventure(t *testing.T) {
+	log.Println("Testing Create Adventure")
+	expectedResponse := &gen.Adventure{
+		UserId:   "652edaa67a75034ea37c6679",
+		Activity: "Test",
+	}
+	adventureRequest := &gen.Adventure{
+		UserId:   "652edaa67a75034ea37c6679",
+		Activity: "Test",
+	}
+
+	createAdventureResp, err := adventureClient.CreateAdventure(ctx, &gen.CreateAdventureRequest{Adventure: adventureRequest})
+	if err != nil {
+		log.Fatalf("Get Adventure: %v", err)
+	}
+
+	if diff := cmp.Diff(createAdventureResp.Adventure.UserId, expectedResponse.UserId, cmpopts.IgnoreUnexported(gen.Adventure{})); diff != "" {
+		log.Fatalf("Get adventure mismatch: %s", diff)
+	}
+
+	adventureId = createAdventureResp.Adventure.AdventureId
+}
+
 func TestGetAdventure(t *testing.T) {
 	log.Println("Testing Get Adventure")
-	ctx := context.Background()
 
 	adventure := &gen.Adventure{
-		AdventureId: "652edaa67a75034ea37c6652",
+		AdventureId: adventureId,
 		UserId:      "652edaa67a75034ea37c6679",
 		Activity:    "Test",
 	}
 
-	getAdventureResp, err := adventureClient.GetAdventure(ctx, &gen.GetAdventureRequest{AdventureId: adventure.AdventureId})
+	adventureResponse, err := adventureClient.GetAdventure(ctx, &gen.GetAdventureRequest{AdventureId: adventure.AdventureId})
 	if err != nil {
 		log.Fatalf("Get Adventure: %v", err)
 	}
-	if diff := cmp.Diff(getAdventureResp.Adventure, adventure, cmpopts.IgnoreUnexported(gen.Adventure{})); diff != "" {
+	if diff := cmp.Diff(adventureResponse.Adventure, adventure, cmpopts.IgnoreUnexported(gen.Adventure{})); diff != "" {
 		log.Fatalf("Get adventure mismatch: %s", diff)
+	}
+}
+
+func TestGetAllAdventures(t *testing.T) {
+	log.Println("Testing Get All Adventures")
+
+	adventure := &gen.Adventure{
+		AdventureId: adventureId,
+		UserId:      "652edaa67a75034ea37c6679",
+		Activity:    "Test",
+	}
+
+	getAdventureResp, err := adventureClient.GetAllAdventures(ctx, &gen.GetAllAdventuresRequest{UserId: adventure.UserId})
+	if err != nil {
+		log.Fatalf("Get All Adventure: %v", err)
+	}
+	if diff := cmp.Diff(getAdventureResp.Adventures[0], adventure, cmpopts.IgnoreUnexported(gen.Adventure{})); diff != "" {
+		log.Fatalf("Get All adventure mismatch: %s", diff)
+	}
+}
+
+func TestUpdateAdventure(t *testing.T) {
+
+	expectedResponse := &gen.Adventure{
+		AdventureId: adventureId,
+		UserId:      "652edaa67a75034ea37c6679",
+		Activity:    "Updated Test",
+	}
+
+	log.Println("Testing Update Adventure")
+	updateAdventureRequest := &gen.Adventure{
+		AdventureId: adventureId,
+		UserId:      "652edaa67a75034ea37c6679",
+		Activity:    "Updated Test",
+	}
+
+	updateAdventureResponse, err := adventureClient.UpdateAdventure(ctx, &gen.UpdateAdventureRequest{Adventure: updateAdventureRequest})
+	if err != nil {
+		log.Fatalf("Update Adventure: %v", err)
+	}
+	if diff := cmp.Diff(updateAdventureResponse.Adventure, expectedResponse, cmpopts.IgnoreUnexported(gen.Adventure{})); diff != "" {
+		log.Fatalf("Update adventure mismatch: %s", diff)
+	}
+}
+
+func TestDeleteAdventure(t *testing.T) {
+	log.Println("Testing Delete Adventure")
+
+	expectedResponse := &gen.Adventure{
+		AdventureId: adventureId,
+		UserId:      "652edaa67a75034ea37c6679",
+		Activity:    "Test",
+	}
+
+	deleteAdventureResponse, err := adventureClient.DeleteAdventure(ctx, &gen.DeleteAdventureRequest{AdventureId: adventureId})
+	if err != nil {
+		log.Fatalf("Delete Adventure: %v", err)
+	}
+	if diff := cmp.Diff(deleteAdventureResponse.AdventureId, expectedResponse.AdventureId, cmpopts.IgnoreUnexported(gen.Adventure{})); diff != "" {
+		log.Fatalf("Delete adventure mismatch: %s", diff)
 	}
 }
--- go.mod
+++ go.mod
@@ -8,8 +8,9 @@ require (
 	github.com/go-faker/faker/v4 v4.2.0
 	github.com/google/go-cmp v0.6.0
 	github.com/joho/godotenv v1.5.1
-	go.mongodb.org/mongo-driver v1.13.0
-	golang.org/x/crypto v0.15.0
+	github.com/mjarkk/mongomock v0.0.0-20230619160045-6439478855a8
+	go.mongodb.org/mongo-driver v1.13.1
+	golang.org/x/crypto v0.17.0
 	google.golang.org/grpc v1.59.0
 	google.golang.org/protobuf v1.31.0
 	gopkg.in/yaml.v3 v3.0.1
@@ -26,27 +27,27 @@ require (
 	github.com/go-playground/validator/v10 v10.16.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/golang/snappy v0.0.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
-	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
 	golang.org/x/arch v0.6.0 // indirect
 	golang.org/x/net v0.18.0 // indirect
-	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
 )
